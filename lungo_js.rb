7185a6049b308a8de62a1066771bc8c79fa3fd8f
Lleïr Borràs Metje <memtest@gmail.com>
2012-01-18 13:16:13 +0100

    get put post delete

--- lungo_js.rb
+++ lungo_js.rb
@@ -2,15 +2,25 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-  haml :index, :format => :html5
-end
-
-get '/:params' do |p|
-  content_type :json
-  { :params => p }.to_json
+  if params.empty?
+    haml :index, :format => :html5
+  else
+    content_type :json
+    {:get => params}.to_json
+  end
 end
 
 post '/' do
   content_type :json
-  { :params => params }.to_json
+  {:post => params}.to_json
+end
+
+put '/' do
+  content_type :json
+  {:put => params}.to_json
+end
+
+delete '/' do
+  content_type :json
+  {:delete => params}.to_json
 end
